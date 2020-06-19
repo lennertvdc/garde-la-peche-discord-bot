8efb614077123fe4d3a8d494b73fb175fb5d4d46
Lennert Vandecasteele <lennert.vandecasteele@outlook.com>
2020-06-19 18:54:59 +0200

    Update .gitignore

---
+++ example.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "token": "YOUR_DISCORD_BOT_TOKEN",
+    "prefix": "!",
+    "channel_name": "garde-la-pêche",
+    "webhook": {
+        "name": "Garde la Pêche",
+        "avatar_url": "https://images.squarespace-cdn.com/content/5d49979b00d3960001160b31/1578400669540-4FD9VBZN997JB5EDV65O/Logo+Peche+FINAL.png?format=1500w&content-type=image%2Fpng"
+    }
+}
