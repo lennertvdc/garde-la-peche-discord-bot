0c859981bced25d960b1b219e2ca9d7818e2a7f0
Lennert Vandecasteele <36887732+lennertvdc@users.noreply.github.com>
2020-06-19 18:55:52 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "token": "NzEzNzYwODk1NDE0NTY2OTYy.XuzTGw.FYTraLtfCOyABYj6o65tKzZaRfU",
-    "prefix": "!",
-    "channel_name": "garde-la-pêche",
-    "webhook": {
-        "name": "Garde la Pêche",
-        "avatar_url": "https://images.squarespace-cdn.com/content/5d49979b00d3960001160b31/1578400669540-4FD9VBZN997JB5EDV65O/Logo+Peche+FINAL.png?format=1500w&content-type=image%2Fpng"
-    }
-}
